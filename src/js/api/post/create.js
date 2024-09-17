64db248aeb1f5b2cc89463efc9fa16c7848c63d4
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2024-09-17 19:44:15 +0200

    started create post

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,1 +1,30 @@
-export async function createPost({ title, body, tags, media }) {}
+import { headers } from "../../api/headers";
+import { API_SOCIAL_POSTS } from "../../api/constants";
+export async function createPost({ title, body, media }) {
+  const data = {
+    title: title,
+    body: body,
+    media: {
+      url: media.url,
+      alt: media.alt,
+    },
+  };
+
+  try {
+    let result = await fetch(API_SOCIAL_POSTS, {
+      method: "POST",
+      headers: headers(),
+      body: JSON.stringify(data),
+    });
+
+    if (!result.ok) {
+      throw new Error(result.message);
+    }
+
+    res = await result.json();
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.error(`Response status: ${result.status}: ${result.statusText} `);
+  }
+}
--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -1,1 +1,8 @@
-export async function onCreatePost(event) {}
+import { createPost } from "../../api/post/create";
+
+export async function onCreatePost(event) {
+  event.preventDefault();
+
+  let createFormInput;
+  console.log(event.target);
+}
