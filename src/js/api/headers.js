b4f2a58a47f7f75e27bf49eee995aa35403d05cf
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2024-09-30 13:50:40 +0200

    js docs added to headers.js

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -1,5 +1,14 @@
 import { API_KEY } from "./constants";
 
+/**
+ * Creates a new Headers object with the Content-Type set to application/json.
+ *
+ * If the API_KEY is set, it is added to the headers as "X-Noroff-API-Key".
+ *
+ * If the user is logged in, the Authorization header is set to "Bearer <token>".
+ *
+ * @returns {Headers} a new Headers object
+ */
 export function headers() {
   const headers = new Headers();
 
